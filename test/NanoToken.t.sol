2a3bf2b3427f4bb9cc20659f3cb1c6357b1ccb33
jiangplus <jiang.plus.times@gmail.com>
2026-02-18 18:25:03 +0800

    feat: scaffold Foundry ERC20 project with OpenZeppelin v5.5.0

---
+++ test/NanoToken.t.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+import {NanoToken} from "src/NanoToken.sol";
+
+contract NanoTokenTest is Test {
+    NanoToken internal token;
+
+    function setUp() public {
+        token = new NanoToken(1_000_000 ether);
+    }
+
+    function testInitialSupplyMintedToDeployer() public view {
+        assertEq(token.totalSupply(), 1_000_000 ether);
+        assertEq(token.balanceOf(address(this)), 1_000_000 ether);
+    }
+
+    function testTransfer() public {
+        address recipient = address(0xBEEF);
+
+        token.transfer(recipient, 100 ether);
+
+        assertEq(token.balanceOf(recipient), 100 ether);
+        assertEq(token.balanceOf(address(this)), 999_900 ether);
+    }
+}
