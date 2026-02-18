68f528764a5db446cf8474a30be94c824ed0191f
jiangplus <jiang.plus.times@gmail.com>
2026-02-18 18:47:51 +0800

    feat: add whitelist-restricted transfer mode with burn support

--- src/NanoToken.sol
+++ src/NanoToken.sol
@@ -2,10 +2,62 @@
 pragma solidity ^0.8.20;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract NanoToken is ERC20, Ownable {
+contract NanoToken is ERC20, ERC20Burnable, Ownable {
+    mapping(address => bool) public blacklisted;
+    mapping(address => bool) public whitelisted;
+    uint256 public whitelistCount;
+
+    error BlacklistedAddress(address account);
+    error WhitelistRestrictedTransfer(address from, address to);
+
+    event BlacklistUpdated(address indexed account, bool isBlacklisted);
+    event WhitelistUpdated(address indexed account, bool isWhitelisted);
+
     constructor(uint256 initialSupply) ERC20("Nano Token", "NANO") Ownable(msg.sender) {
         _mint(msg.sender, initialSupply);
+    }
+
+    function setBlacklist(address account, bool isBlacklisted) external onlyOwner {
+        blacklisted[account] = isBlacklisted;
+        emit BlacklistUpdated(account, isBlacklisted);
+    }
+
+    function setWhitelist(address account, bool isWhitelisted) external onlyOwner {
+        bool current = whitelisted[account];
+        if (current == isWhitelisted) {
+            return;
+        }
+
+        whitelisted[account] = isWhitelisted;
+        if (isWhitelisted) {
+            whitelistCount += 1;
+        } else {
+            whitelistCount -= 1;
+        }
+        emit WhitelistUpdated(account, isWhitelisted);
+    }
+
+    function _update(address from, address to, uint256 value) internal virtual override {
+        if (from != address(0) && blacklisted[from]) {
+            revert BlacklistedAddress(from);
+        }
+        if (to != address(0) && blacklisted[to]) {
+            revert BlacklistedAddress(to);
+        }
+
+        if (from != address(0) && whitelistCount > 0 && !_isWhitelistExemptSender(from)) {
+            if (to != address(0) && !whitelisted[to]) {
+                revert WhitelistRestrictedTransfer(from, to);
+            }
+        }
+
+        super._update(from, to, value);
+    }
+
+    function _isWhitelistExemptSender(address sender) internal view returns (bool) {
+        return sender == owner() || whitelisted[sender];
     }
 }
--- test/NanoToken.t.sol
+++ test/NanoToken.t.sol
@@ -6,6 +6,9 @@ import {NanoToken} from "src/NanoToken.sol";
 
 contract NanoTokenTest is Test {
     NanoToken internal token;
+    address internal user = address(0xBEEF);
+    address internal recipient = address(0xCAFE);
+    address internal allowedRecipient = address(0xABCD);
 
     function setUp() public {
         token = new NanoToken(1_000_000 ether);
@@ -21,11 +24,113 @@ contract NanoTokenTest is Test {
     }
 
     function testTransfer() public {
-        address recipient = address(0xBEEF);
-
         token.transfer(recipient, 100 ether);
 
         assertEq(token.balanceOf(recipient), 100 ether);
         assertEq(token.balanceOf(address(this)), 999_900 ether);
+    }
+
+    function testOwnerCanSetAndUnsetBlacklist() public {
+        token.setBlacklist(user, true);
+        assertTrue(token.blacklisted(user));
+
+        token.setBlacklist(user, false);
+        assertFalse(token.blacklisted(user));
+    }
+
+    function testNonOwnerCannotManageBlacklist() public {
+        vm.prank(user);
+        vm.expectRevert();
+        token.setBlacklist(user, true);
+
+        vm.prank(user);
+        vm.expectRevert();
+        token.setBlacklist(user, false);
+    }
+
+    function testBlacklistedSenderCannotTransfer() public {
+        token.transfer(user, 100 ether);
+        token.setBlacklist(user, true);
+
+        vm.prank(user);
+        vm.expectRevert(abi.encodeWithSelector(NanoToken.BlacklistedAddress.selector, user));
+        token.transfer(recipient, 1 ether);
+    }
+
+    function testBlacklistedRecipientCannotReceive() public {
+        token.setBlacklist(recipient, true);
+
+        vm.expectRevert(abi.encodeWithSelector(NanoToken.BlacklistedAddress.selector, recipient));
+        token.transfer(recipient, 1 ether);
+    }
+
+    function testOwnerCanSetAndUnsetWhitelist() public {
+        token.setWhitelist(user, true);
+        assertTrue(token.whitelisted(user));
+        assertEq(token.whitelistCount(), 1);
+
+        token.setWhitelist(user, false);
+        assertFalse(token.whitelisted(user));
+        assertEq(token.whitelistCount(), 0);
+    }
+
+    function testNonOwnerCannotManageWhitelist() public {
+        vm.prank(user);
+        vm.expectRevert();
+        token.setWhitelist(user, true);
+    }
+
+    function testWhitelistEmptyDoesNotRestrictTransfers() public {
+        token.transfer(user, 10 ether);
+
+        vm.prank(user);
+        token.transfer(recipient, 1 ether);
+        assertEq(token.balanceOf(recipient), 1 ether);
+    }
+
+    function testNonWhitelistedSenderCannotTransferToNonWhitelistedRecipient() public {
+        token.transfer(user, 10 ether);
+        token.setWhitelist(allowedRecipient, true);
+
+        vm.prank(user);
+        vm.expectRevert(
+            abi.encodeWithSelector(NanoToken.WhitelistRestrictedTransfer.selector, user, recipient)
+        );
+        token.transfer(recipient, 1 ether);
+    }
+
+    function testNonWhitelistedSenderCanTransferToWhitelistedRecipient() public {
+        token.transfer(user, 10 ether);
+        token.setWhitelist(allowedRecipient, true);
+
+        vm.prank(user);
+        token.transfer(allowedRecipient, 1 ether);
+        assertEq(token.balanceOf(allowedRecipient), 1 ether);
+    }
+
+    function testNonWhitelistedSenderCanBurnWhenWhitelistIsNotEmpty() public {
+        token.transfer(user, 10 ether);
+        token.setWhitelist(allowedRecipient, true);
+
+        vm.prank(user);
+        token.burn(1 ether);
+        assertEq(token.balanceOf(user), 9 ether);
+    }
+
+    function testOwnerCanAlwaysTransferWhenWhitelistIsNotEmpty() public {
+        token.setWhitelist(allowedRecipient, true);
+
+        token.transfer(recipient, 1 ether);
+        assertEq(token.balanceOf(recipient), 1 ether);
+    }
+
+    function testWhitelistedSenderCanAlwaysTransferWhenWhitelistIsNotEmpty() public {
+        token.transfer(user, 10 ether);
+        token.setWhitelist(user, true);
+        token.setWhitelist(allowedRecipient, true);
+
+        vm.prank(user);
+        token.transfer(recipient, 1 ether);
+        assertEq(token.balanceOf(recipient), 1 ether);
     }
 }
